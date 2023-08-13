654aeb08a358360a1376fcf62ca1d874ad0083c5
ningyuan <1900839929@qq.com>
2023-08-13 22:48:41 +0800

    [add]第二次提交记录

---
+++ blog-front/src/main/java/com/ny/blogApplication.java
@@ -0,0 +1,13 @@
+package com.ny;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 启动类
+@SpringBootApplication
+public class blogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(blogApplication.class,args);
+    }
+}
